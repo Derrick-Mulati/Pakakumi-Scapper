f9d313cc5e1821df8005c30e2ab078ebbb064f8b
Derrick-Mulati <mulatiderrick@gmail.com>
2023-12-07 11:34:25 +0300

    Latest Updates

---
+++ index.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+try:
+    source = requests.get('https://www.betpawa.co.ke/virtual-sports?virtualTab=results')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+
+    out = soup.find('div', class_="l-container")
+
+    print(out)
+
+
+
+
+
+
+
+
+except Exception as e:
+    print(e)
